e80ef19b15b27e0623804767291187d75eed90bf
Kaija Hall <kaija1342@gmail.com>
2026-03-04 05:51:27 -0500

    Edited info3180-lab3 files

--- app/config.py
+++ app/config.py
@@ -7,3 +7,8 @@ class Config(object):
     """Base Config Object"""
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    MAIL_SERVER="sandbox.smtp.mailtrap.io"
+    MAIL_PORT=2525
+    MAIL_USERNAME="2b4a55b5d491e8"
+    MAIL_PASSWORD="503f2262c33deb"
+    SECRET_KEY="Random#765"
